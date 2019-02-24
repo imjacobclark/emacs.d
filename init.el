0cd15898ab822d4d13f20c0af959410b0d7d2d42
Jacob Clark <jacob.clark@bbc.co.uk>
2019-02-24 17:30:47 +0000

    Update init.el

--- init.el
+++ init.el
@@ -15,8 +15,11 @@ There are two things you can do about this warning:
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+
 (package-initialize)
-      
+(package-refresh-contents)
+(package-install "tide")
+
 (require 'linum)
 
 ;; Setup for OSX
